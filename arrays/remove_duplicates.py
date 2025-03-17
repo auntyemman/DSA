f28677cf50b9ee5b42abc2456b573c96e039bf6d
auntyemman <1mole.coco@gmail.com>
2025-03-17 23:03:58 +0100

    python: remove duplicates from a sorted array

--- arrays/remove_duplicates.py
+++ arrays/remove_duplicates.py
@@ -1,7 +1,7 @@
 #remove dduplicates from a sorted array
 
 # method 1, stepping back after seeing duplicates
-# Time complexity: O(n)
+# Time complexity: O(n^2)
 # Space complexity: O(1)
 def remove_duplicates(nums):
     i = 0
@@ -11,6 +11,20 @@ def remove_duplicates(nums):
             i = i - 1
         i = i + 1
     return len(nums)
+
+
+# method 2, stepping back after seeing duplicates
+# Time complexity: O(n)
+# Space complexity: O(1)
+def remove_duplicates(nums):
+    k = 1
+    i = 1
+    while (i < len(nums)):
+        if (nums[i] != nums[k-1]):
+            nums[k] = nums[i]
+            k = k + 1
+        i = i + 1
+    return k
 
 print(remove_duplicates([1,1,2]))
 print(remove_duplicates([0,0,1,1,1,2,2,3,3,4]))
---
+++ arrays/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+def remove_duplicates(nums):
+#     i = 0
+#     while (i < len(nums) - 1):
+#         if (nums[i] == nums[i+1]):
+#             del nums[i]
+#             i = i - 1
+#         i = i + 1
+#     return len(nums)
