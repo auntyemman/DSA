cecbc4ffa8e0dcdd1b272ba5d56b7d94501c327b
auntyemman <1mole.coco@gmail.com>
2025-03-17 23:12:31 +0100

    javascript: remove duplicates from a sorted array

--- arrays/remove_duplicates.js
+++ arrays/remove_duplicates.js
@@ -14,7 +14,7 @@ function removeDuplicates(nums) {
     return nums.length
 }
 
-// method 2, stepping back after seeing duplicates
+// method 2, replacing elements with its unique value
 // Time complexity: O(n)
 // Space complexity: O(1)
 
@@ -31,6 +31,22 @@ function removeDuplicates(nums) {
     }
     return k
 }
+
+// method 3, tracks frequency in a map
+// Time complexity: O(n)
+// Space complexity: O(n)
+function removeDuplicates(nums) {
+    let tracker = {}
+    for(let i=0; i<nums.length; i++){
+        tracker[nums[i]] = (tracker[nums[i]] || 0) + 1
+    }
+    for(let i=0; i<nums.length; i++){
+        if (nums[i] === nums[i+1]){
+            nums.splice(i, tracker[nums[i]] - 1)
+        }
+    }
+    return nums.length
+};
 
 console.log(removeDuplicates([1,1,2]));
 console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
