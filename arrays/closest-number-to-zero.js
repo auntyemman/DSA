281c98a124a543eecdadad9d18b93dc5aa7b418b
auntyemman <1mole.coco@gmail.com>
2025-04-14 09:30:31 +0100

    closest number to zero: javascript

--- arrays/closest-number-to-zero.js
+++ arrays/closest-number-to-zero.js
@@ -1,12 +1,16 @@
-function closestToZero(nums){
-    let closeToZero = Math.abs(nums[0]);
-    let i = 0;
-    while(i < nums.length-1){
-        let absNum = Math.abs(nums[i]);
-        closeToZero = Math.min(absNum, closeToZero);
-        i++;
+function closestToZero(nums) {
+    let closest = nums[0];
+    for(let i=1; i<nums.length; i++){
+        const absNum = Math.abs(nums[i]);
+        const absClosest = Math.abs(closest);
+        if(absNum < absClosest) {
+            closest = nums[i];
+        }
+        else if (absNum === absClosest && nums[i] > closest){
+            closest = nums[i];
+        }
     }
-    return closeToZero;
+    return closest;
 }
-
 console.log(closestToZero([-4,-2,1,4,8]));
+console.log(closestToZero([-1000, -1000]));
